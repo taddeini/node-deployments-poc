aef81be54e7008c0e1c9e0a294cc3d1a30b8fc3c
pgreens <info+github@paulgmail.com>
2021-06-11 07:59:44 -0500

    Set up Docker configuration

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const morgan = require("morgan");
-const host = process.env.HOST || "127.0.0.1";
+const express = require('express');
+const morgan = require('morgan');
+// port change helped
+const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
-app.get("/", (re, res) => {
-  res.json("Hello node-deployment-poc!");
+app.get('/', (re, res) => {
+  res.json('Hello node-deployment-poc!');
 });
 
 app.listen(port, host, () => {
